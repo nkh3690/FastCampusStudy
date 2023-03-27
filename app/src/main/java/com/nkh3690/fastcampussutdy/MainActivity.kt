cd6a04c0ab51aaab74cc84312be55f6a03e2cabd
남기훈 <nkh3690@naver.com>
2023-03-27 22:23:53 +0900

    최초 커밋

---
+++ app/src/main/java/com/nkh3690/fastcampussutdy/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.nkh3690.fastcampussutdy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
